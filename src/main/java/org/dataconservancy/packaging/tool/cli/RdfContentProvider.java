ed0cb57bc1161d4f13cad4fee92c17149b299519
Ben Trumbore <ben@trumbore.com>
2016-12-09 17:26:28 -0500

    Initial implementation of RdfContentProvider
    Following on the refactoring of IpmPackager that created the
    ContentProvider abstract class, this submission creates an
    RdfContentProvider implementation of that class.  This implementation is
    exercised by RdfContentProviderTest, which uses the resource data files
    in this submission.

---
+++ src/main/java/org/dataconservancy/packaging/tool/cli/RdfContentProvider.java
@@ -0,0 +1,223 @@
+/*
+ * Copyright 2016 Johns Hopkins University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dataconservancy.packaging.tool.cli;
+
+import org.apache.jena.rdf.model.*;
+
+import org.dataconservancy.packaging.shared.ContentProvider;
+import org.dataconservancy.packaging.tool.model.ipm.FileInfo;
+import org.dataconservancy.packaging.tool.model.ipm.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Implementation of ContentProvider for RDF sourced data.
+ * Created by Ben Trumbore on 12/1/2016.
+ */
+public class RdfContentProvider extends ContentProvider {
+
+    private Logger  LOG = LoggerFactory.getLogger(RdfContentProvider.class);
+    private Model   domainObjects = null;
+    private URI     contentPath;
+
+    /**
+     * Create a content provider that can be passed to IpmPackager.buildPackage.
+     * @param domainObjs The RDF model of the domain objects.
+     * @param contentURI A URL to the files corresponding to the domain model.
+     */
+    public RdfContentProvider(Model domainObjs, URI contentURI) {
+        domainObjects = domainObjs;
+        contentPath = contentURI;
+    }
+
+    /**
+     * Returns the domain model provided in the constructor.
+     * @return The Model representing the domain objects
+     */
+    public Model getDomainModel() {
+        return domainObjects;
+    }
+
+    // Define properties that will be inspected for some RDF nodes.
+    private static Property hasType     = ResourceFactory.createProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
+    private static Property hasTitle    = ResourceFactory.createProperty("http://dataconservancy.org/business-object-model#hasTitle");
+    private static Property metadataFor = ResourceFactory.createProperty("http://dataconservancy.org/business-object-model#metadataFor");
+    private static Property isMemberOf  = ResourceFactory.createProperty("http://dataconservancy.org/business-object-model#isMemberOf");
+
+    /*
+     * TODO - Update doc throughout this method
+     * We manually build the IPM tree here. Fundamentally, we're doing three things:
+     * 1) Creating "directory" nodes that correspond to a domain object.
+     * 2) Creating "content" nodes that correspond to a domain object that
+     *    describes associated content.
+     * 3) Arranging these nodes into a tree structure of our liking.
+     */
+    public Node getIpmModel() {
+        // Must use an array to allow root node to be set inside lambda function below.
+        final Node[] root = new Node[1];
+
+        String msgFmt = "Creating %s IPM node named '%s' for domain object %s";
+
+        // For each subject resource that is not anonymous, create a node.
+        // - If the node type is an Image or File, then it will be a content node.
+        // - Otherwise, make a directory node.
+        // It is assumed that all folders will precede their child nodes/folders.
+        domainObjects.listSubjects().forEachRemaining(subject -> {
+            if (subject.isAnon()) {
+                LOG.debug("Skipping IPM node creation for anonymous resource '{}'", subject.getId().toString());
+                return;
+            }
+
+            URI u = URI.create(subject.getURI());
+
+            // Hash URIs do not get their own node; they will be considered to be a single node.
+            // TODO - Is this needed for JenaModel input? (copied from buildModelTree)
+            if (u.getFragment() != null) {
+                LOG.debug("Skipping IPM node creation for hash URI resource '{}'", subject.getURI());
+                return;
+            }
+
+            // What type of node is this?  What is its name?
+            String type  = subject.getProperty(hasType ).getObject().toString();
+            String title = subject.getProperty(hasTitle).getObject().toString();
+            boolean isImage  = type.equals("http://dataconservancy.org/business-object-model#Metadata");
+            boolean isFile   = type.equals("http://dataconservancy.org/business-object-model#File");
+            boolean isFolder = type.equals("http://dataconservancy.org/business-object-model#DataItem");
+
+            // Create the new package tree node and insert it into the tree.
+            Node n = new Node(u);
+            n.setDomainObject(u);
+
+            // Assign node file info and parent depending on node type.
+            // Note that file info must be assigned before finding/assigning parent.
+            if (isImage || isFile) {
+                LOG.info(String.format(msgFmt, "file", title, subject.getURI()));
+                Property prop = isImage ? metadataFor : isMemberOf;
+                String parentID = subject.getProperty(prop).getObject().toString();
+                assignFileInfo(title, parentID, contentPath, domainObjects, n, true);
+                assignParentNode(n, root[0]);
+            }
+            else if (isFolder) {
+                LOG.info(String.format(msgFmt, "directory", title, subject.getURI()));
+                String parentID = subject.getProperty(isMemberOf).getObject().toString();
+                assignFileInfo(title, parentID, contentPath, domainObjects, n, false);
+                assignParentNode(n, root[0]);
+            }
+            else { // root folder, type == "Collection"
+                LOG.info(String.format(msgFmt, "root directory", title, subject.getURI()));
+                assignFileInfo(title, null, contentPath, domainObjects, n, false);
+                root[0] = n;
+            }
+        });
+
+        return root[0];
+    }
+
+    /**
+     * Create a FileInfo that points to file/folder content found in content tree path.
+     *
+     * @param fileName The name of the file or folder to be included.
+     * @param parentID ID of the node's parent in the domain model.
+     * @param contentPath Path to the root of the local package data tree.
+     * @param domainObjects The domain model tree.
+     * @param node The node for which the FileInfo is being created.
+     * @param isFile True if the node represents a file, False if it is a folder.
+     */
+    private void assignFileInfo(String fileName, String parentID, URI contentPath,
+                                       Model domainObjects, Node node, boolean isFile) {
+        String localPath = (parentID == null)
+                ? fileName
+                : getPath(fileName, parentID, domainObjects);
+        Path fullPath = Paths.get(contentPath + "/" + localPath);
+
+        FileInfo info = new FileInfo(fullPath);
+        if (isFile)
+            info.setIsFile(true);
+        else
+            info.setIsDirectory(true);
+        node.setFileInfo(info);
+    }
+
+    /**
+     * Creates a complete path from the tail end of a path up to the domain model root.
+     * Prepends the current path's parent name and
+     * recurses until the domain model does not contain a higher parent.
+     * @param pathTail When first called, the leaf file/folder of the desired path.
+     *                 During recursion, the accumulated path from the leaf upwards.
+     * @param parentID The ID of the domain model node that is the parent of the pathTail.
+     * @param domainObjects The domain model.
+     * @return The complete path from domain model root to the provided leaf.
+     */
+    private String getPath(String pathTail, String parentID, Model domainObjects) {
+        Resource parent = domainObjects.getResource(parentID);
+        String parentName = parent.getProperty(hasTitle).getObject().toString();
+        String newPath  = parentName + "/" + pathTail;
+
+        Statement grandparent = parent.getProperty(isMemberOf);
+        if (grandparent != null)
+            return getPath(newPath, grandparent.getObject().toString(), domainObjects);
+        else
+            return newPath;
+    }
+
+    /**
+     * Finds and assigns the parent IPM tree node of the given node.
+     * @param node The node for which a parent is found and assigned.
+     * @param root The root node of the IPM tree.
+     */
+    private void assignParentNode(Node node, Node root) {
+        // Find the file system paths to the provided root and target nodes.
+        String nodeLoc = node.getFileInfo().getLocation().getPath();
+        nodeLoc = nodeLoc.replaceFirst("^/(.:/)", "$1"); // Remove leading "/" on Windows
+        Path nodePath = Paths.get(nodeLoc);
+        String rootLoc = root.getFileInfo().getLocation().getPath();
+        rootLoc = rootLoc.replaceFirst("^/(.:/)", "$1"); // Remove leading "/" on Windows
+        Path rootPath = Paths.get(rootLoc);
+
+        // Find the relative file system path between the root and target nodes.
+        Path relPath  = rootPath.relativize(nodePath);
+
+        // Walk the tree from the root node down through its children
+        // until the target node is reached.  At each step find the child whose name
+        // matches the first name in the remaining relative path.
+        Node parent = root;
+        while (relPath.getNameCount() > 1) {
+            String name = relPath.getName(0).toString();
+            List<Node> children = parent.getChildren();
+            Node match = null;
+            for (Node child : children) {
+                if (child.getFileInfo().getName().equals(name)) {
+                    match = child;
+                    break;
+                }
+            }
+            if (match == null)
+                throw new RuntimeException("Expected IPM model folder '" + name + "' does not exist!");
+            parent = match;
+            relPath = relPath.subpath(1, relPath.getNameCount());
+        }
+
+        // Assign the target node's discovered parent.
+        parent.addChild(node);
+    }
+
+}
---
+++ src/test/java/org/dataconservancy/packaging/tool/cli/RdfContentProviderTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2016 Johns Hopkins University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dataconservancy.packaging.tool.cli;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.junit.Test;
+
+import org.dataconservancy.packaging.shared.IpmPackager;
+
+import java.io.InputStream;
+import java.net.URI;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+
+/**
+ * Tests for validating RDF-sourced content in a DC package.
+ * Created by Ben Trumbore on 12/6/2016.
+ */
+public class RdfContentProviderTest {
+
+    // Create a Jena model, package metadata and generation parameters
+    // to pass to an IpmPackager to create a package.
+    // Compare the package contents to that of the original source.
+    @Test
+    public void testCreateRdfPackage() throws Exception {
+        String dataResource = "/testCreateRdfPackage/";
+        String jenaResource = dataResource + "state/DOMAIN_OBJECTS";
+        String bagResource  = dataResource + "Hanh-test/data/bin";
+
+        // Read Jena model from sample DOMAIN_OBJECTS file.
+        String jenaFile = RdfContentProviderTest.class.getResource(jenaResource).getPath();
+        Model jenaModel = ModelFactory.createDefaultModel();
+        RDFDataMgr.read(jenaModel, jenaFile, Lang.TTL);
+        assertEquals(148, jenaModel.size());
+
+        // Metadata
+        InputStream metadataStream =
+                RdfContentProviderTest.class.getResourceAsStream("/metadata.properties");
+
+        // Read package generation parameters from a resource file.
+        InputStream paramStream =
+                RdfContentProviderTest.class.getResourceAsStream("/PackageGenerationParams.properties");
+
+        String contentPath = RdfContentProviderTest.class.getResource(bagResource).getPath();
+        contentPath = contentPath.replaceFirst("^/(.:/)", "$1"); // Remove leading "/" on Windows
+        URI contentURI = new URI(contentPath);
+
+        RdfContentProvider contentProvider = new RdfContentProvider(jenaModel, contentURI);
+
+        // Create the package
+        IpmPackager packager = new IpmPackager();
+        org.dataconservancy.packaging.tool.api.Package pkg =
+                packager.buildPackage(contentProvider, metadataStream, paramStream);
+
+        // TODO - Test the package contents
+        assertNotNull(pkg);
+    }
+
+}
